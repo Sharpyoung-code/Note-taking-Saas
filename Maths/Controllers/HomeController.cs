e05382418ccdff426c9f86d01b9cd1fb8cad0498
Okoye Emmanuel <mmesoemma@gmail.com>
2020-05-07 01:44:10 +0100

    Initial

--- Maths/Controllers/HomeController.cs
+++ Maths/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Maths.Models;
+using System.Threading.Tasks;
 
 namespace Maths.Controllers
 {
@@ -25,6 +27,41 @@ namespace Maths.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+
+        }
+        public ActionResult Solve(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Solve(string firstNumber, string secondNumber, string returnUrl)
+        {
+            double FirstNumber = Math.Sqrt(Int32.Parse(firstNumber));
+            double SecondNumber = Math.Sqrt(Int32.Parse(secondNumber));
+            var model = new Solve();
+
+            if(FirstNumber < 0 || SecondNumber < 0)
+            {
+                model.ErrorMessage = "One or two of the numbers you entered was negative. Please put another number";
+                return View(model);
+            }
+            if(FirstNumber > SecondNumber)
+            {
+                model.SuccessMessage = "The number " + " " + firstNumber + " " + "with Square root " + " " + FirstNumber + " " + "has a higher square root than the number " + " " + secondNumber + " " + "with square root " + " " + SecondNumber + ".";
+                return View(model);
+            }
+            else if(FirstNumber == SecondNumber)
+            {
+                model.ErrorMessage = "The 2 numbers you entered has the same square root. Please enter another value";
+                return View(model);
+            }
+            else
+            {
+                model.SuccessMessage = "The number " + " " + secondNumber + " " + "with Square root " + " " + SecondNumber + " " + "has a higher square root than the number " + " " + firstNumber + " " + "with square root " + " " + FirstNumber + ".";
+                return View(model);
+            }
+
         }
     }
 }
---
+++ Maths/Models/Solve.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Maths.Models
+{
+    public class Solve
+    {
+        [Display(Name = "Enter your First Number")]
+        [Required(ErrorMessage = "Enter a valid number")]
+        public string firstNumber { get; set; }
+        [Display(Name = "Enter your Second Number")]
+        [Required(ErrorMessage = "Enter a valid number")]
+        public string secondNumber { get; set; }
+        public string ErrorMessage { get; set; }
+        public string SuccessMessage { get; set; }
+    }
+}
